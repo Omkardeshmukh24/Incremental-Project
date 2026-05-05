615c10e08d91711639ad36574f8170d6639ddf56
Omkar Deshmukh <omkardeshmukh164@gmail.com>
2026-05-05 17:19:13 +0000

    Day 20-25

--- client/src/app/supplylink/types/Warehouse.ts
+++ client/src/app/supplylink/types/Warehouse.ts
@@ -1,26 +1,23 @@
 import { Supplier } from "./Supplier";
+
 export class Warehouse {
     warehouseId: number;
-    supplierId: string;
+    supplier: Supplier;
     warehouseName: string;
     location: string;
     capacity: number;
-    constructor(warehouseId: any,supplierId: any,warehouseName: any,location: any,capacity: any){
+
+    constructor(
+        warehouseId: number,
+        supplier: Supplier,
+        warehouseName: string,
+        location: string,
+        capacity: number
+    ) {
         this.warehouseId = warehouseId;
-        this.supplierId = supplierId;
+        this.supplier = supplier;
         this.warehouseName = warehouseName;
         this.location = location;
         this.capacity = capacity;
     }
-
-    displayInfo():void{
-        console.log("Warehouse ID : " + this.warehouseId);
-        console.log("Supplier ID : " + this.supplierId);
-        console.log("Warehouse Name : " + this.warehouseName);
-        console.log("Location : " + this.location);
-        console.log("Capacity : " + this.capacity);
-        
-    }
-
- 
 }
